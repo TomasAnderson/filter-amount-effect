33c43d68c7cb8fbdd425d86b58cb5c0c8e183a76
Yuri Zhou <t_zhouy@ads.autodesk.com>
2016-06-02 18:03:42 +0800

    first try

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 var React = require('react');
+var ExpandableDiv = require('./expandableDiv.js');
 
 var App = React.createClass({
+  getInitialStage: function () {
+    return {
+      grids: this.props.grids
+    };
+  },
 	render: function () {
-		return <h1>Hello World!</h1>	  
+    return (
+      <div className="demo">
+        <div className="grids">
+          {this.props.grids.map(function (grid, index) {
+            return (
+              <ExpandableDiv
+                style={{color: grid.color}}
+                name={grid.name}
+                key={index}
+              >
+              </ExpandableDiv>
+            );
+          })}
+        </div>
+      </div>
+    );
 	}
 });
 
---
+++ src/expandableDiv.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var ExpandableDiv = React.createClass({
+  render: function () {
+     return (
+      <div
+        className="ball"
+        style={this.props.style}
+        // onClick={this.props.handleRemove}
+      >
+        <span className="number" style={{visibility: 'hidden'}}>{this.props.name}</span>
+      </div>  
+    );
+  }
+});
+
+module.exports = ExpandableDiv;
