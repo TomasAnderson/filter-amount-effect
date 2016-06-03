0bf4dcbf11a97590d0bd58db2e47a0d2734af174
Yuri Zhou <t_zhouy@ads.autodesk.com>
2016-06-03 11:39:10 +0800

    finish grids display and filter logic

--- src/App.js
+++ src/App.js
@@ -2,29 +2,64 @@ var React = require('react');
 var ExpandableDiv = require('./expandableDiv.js');
 
 var App = React.createClass({
-  getInitialStage: function () {
+  getInitialState: function () {
     return {
-      grids: this.props.grids
+      filterOption: 'All'
     };
   },
+
+  onFilterChange: function (e) {
+    this.setState({
+      filterOption: e.target.value
+    });
+  },
+
 	render: function () {
     return (
       <div className="demo">
+        <div className="filter">
+          <select value={this.state.filterOption} onChange={this.onFilterChange}>
+            <option value="All">All</option>
+            <option value="Warm">Warm Color First</option>
+            <option value="Cold">Cold Color First</option>
+          </select>
+        </div>
         <div className="grids">
-          {this.props.grids.map(function (grid, index) {
-            return (
-              <ExpandableDiv
-                style={{color: grid.color}}
-                name={grid.name}
-                key={index}
-              >
-              </ExpandableDiv>
-            );
-          })}
+          {this._getGrids()}
         </div>
       </div>
     );
-	}
+	},
+
+  _getGrids: function () {
+    var contents = [];
+    var grids = this.props.grids;
+    for (var i=0; i<grids.length; i+=3) {
+      var left = (<ExpandableDiv 
+        color={grids[i].color}
+        name={grids[i].name}
+        key={i}
+      ></ExpandableDiv>);
+      var middle = i+1<grids.length ? (<ExpandableDiv
+          color={grids[i+1].color}
+          name={grids[i+1].name}
+          key={i+1}
+        ></ExpandableDiv>) : null;
+      var right = i+2<grids.length ? (<ExpandableDiv
+          color={grids[i+2].color}
+          name={grids[i+2].name}
+          key={i+2}
+        ></ExpandableDiv>) : null;
+      contents.push(
+        <div className="row" key={i}>
+          {left}{middle}{right}
+        </div>
+      );
+    }
+    return contents;
+  }
+  
+
 });
 
 module.exports = App;
--- src/expandableDiv.js
+++ src/expandableDiv.js
@@ -4,8 +4,8 @@ var ExpandableDiv = React.createClass({
   render: function () {
      return (
       <div
-        className="ball"
-        style={this.props.style}
+        className="grid"
+        style={{'background-color': this.props.color}}
         // onClick={this.props.handleRemove}
       >
         <span className="number" style={{visibility: 'hidden'}}>{this.props.name}</span>
